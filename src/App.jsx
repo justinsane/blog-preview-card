68fe7fa256c495f04f5a018457cdc1b55d1035d7
Justin Levy <justin@justlevy.com>
2024-07-01 20:11:27 -0700

    changed styles and html

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className='mx-6 mt-24 max-w-sm md:mx-auto py-5 px-6 rounded-2xl outline outline-1  bg-white box-shadow-container'>
+    <div className='mx-5 max-w-sm md:mx-auto py-5 px-6 rounded-2xl outline outline-1 bg-white box-shadow-container'>
       <img
         src='/illustration-article.svg'
         alt='Blog Image'
         className='mx-auto rounded-xl'
       />
-      <h2 className='text-sm font-bold '>
-        <span className='bg-yellow py-1 px-3 rounded inline-block mt-6'>
-          Learning
-        </span>
-      </h2>
-      <h3 className='text-medium mt-2'>Published 21 Dec 2023</h3>
+      <p className='text-sm font-bold inline-block bg-yellow py-1 px-3 rounded mt-6'>
+        Learning
+      </p>
+      <p className='text-medium mt-2'>Published 21 Dec 2023</p>
       <a href=''>
         <h1 className='font-bold text-2xl mt-3 leading-9'>
           HTML & CSS foundations
